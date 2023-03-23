cf4ea4eee0b1b52d8885ef8efe88dda11b282d53
ashishshrm12 <ashishsharmamngr@gmail.com>
2023-03-23 14:54:16 +0530

    Adding this file

---
+++ tut1.cpp
@@ -0,0 +1,10 @@
+//BASIC STRUCTURE IN C++
+
+
+
+#include<iostream>
+
+int main(){
+    std::cout <<"hello world";
+    return 0;
+}
---
+++ tut2-3.cpp
@@ -0,0 +1,18 @@
+//COMMENTS
+
+// comments ko kuch likhne ke liye use karte hai and comments likha jaye par code pe kuch farak nahi pade.
+// isko lahane  ke liye humko double forward slash('//') ka use karte hai ya fir slash-star and star-slash ka use karte hai.
+// for rxample- (/* any data */)
+
+
+
+#include<iostream>
+
+/*tgusui osuilh   sq cvgaioiqem mioeent 
+hygtewtigbw
+hi this is ashis sharma 
+and i am new here and enjoying c++*/
+int main(){
+    std::cout<<"Hello World";
+    return 0; 
+} 
